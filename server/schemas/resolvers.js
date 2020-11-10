90e9b62a88aff5470072e51a6492b2a322fb4676
Kristen M <kristenmayorga@gmail.com>
2020-11-09 19:55:06 -0500

    login updates

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,8 +6,28 @@ const User = require ("../models/User");
 
 const resolvers = {
   Query: {
-    // logic here remains the same
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+      users: async () => {
+      return User.find()
+        .select('-__v -password')
+    
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
   },
+},
+
   Mutation: {
     addUser: async (parent, args) => {
       console.log(args);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,9 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Query {
+    me: User
     users: [User]
+    user(username: String!): User
   }
   type Mutation {
     login(email: String!, password: String!): Auth
