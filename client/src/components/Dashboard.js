07a64c5f95c230508696ca2c0084edabed53c0f2
Kristen M <kristenmayorga@gmail.com>
2020-11-09 19:52:07 -0500

    login /signup

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import MenuSidebar from "../components/MenuSidebar";
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  List,
+  Menu,
+  Segment,
+} from "semantic-ui-react";
+
+
 
 function Dashboard() {
     return (
-        <div>
-            Dashboard
-        </div>
-    )
+      <>
+      <MenuSidebar/>
+
+      </>
+    );
 }
 
-export default Dashboard
+export default Dashboard;
---
+++ client/src/components/Friendlist.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { Grid, Header, Segment, List } from "semantic-ui-react";
+
+// import "semantic-ui-css/semantic.min.css";
+
+
+  
+
+//   return (
+//     <>
+   
+//     </>
+//   );
+// ;
+
+// export default FriendList;
