51c2f3eda37b0107de38b08e09cfce1bd63c1643
Stephon Smith <smithwrestling89@gmail.com>
2020-11-12 19:19:57 -0500

    updated typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,5 @@
 const { gql } = require('apollo-server-express');
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    posts: [Post]
-  }
   type Post {
     _id: ID
     postText: String
